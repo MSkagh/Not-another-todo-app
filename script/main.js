e16e708a12f6c899e51b6c2f24d4bb248a42770e
Martin Skagh <martin.skagh@gmail.com>
2024-06-08 23:58:10 +0200

    back to basics

--- script/main.js
+++ script/main.js
@@ -23,49 +23,37 @@ let todos = JSON.parse(localStorage.getItem("todos")) || [];
 let lists = JSON.parse(localStorage.getItem("lists")) || [];
 let selectedList = document.querySelector("[data-selected=true]")
 
-function render(){
-    
-    while(todoList.firstChild){
-        todoList.removeChild(todoList.firstChild)
-    }
-    while(listList.firstChild){
-        listList.removeChild(listList.firstChild)
-    }
-    for (const item of lists){
-        const li = document.createElement("li")
-        li.innerHTML = item
-        listList.appendChild(li);
-    }
-    selectedList = document.querySelector("[data-selected=true]")
-    console.log(selectedList.innerHTML)
+for (const item of lists) {
+    createListItem(item)
 }
-render()
+
 const addTodoButtonAction = () => {
     console.log(selectedList)
 }
 
 const addListButtonAction = () => {
     const name = listInput.value
+    createListItem(name)
+    lists.push(name);
+    persistChanges();
+    listInput.value = "";
+}
+
+function createListItem(name) {
     const li = document.createElement("li")
     li.addEventListener("click", (e) => onListClick(e))
     li.dataset.selected = true
     li.dataset.name = name
+    li.innerHTML = name
     listList.appendChild(li);
-    lists.unshift(name);
-    persistChanges();
-    listInput.value = "";
-    render();
 }
 
-function onListClick(e){
-    for (const li of listList){
-        li.dataset.selected = false
-    }
-    e.target.dataset.selected = true
+function onListClick(e) {
+    console.log("Clicked")
 }
 
-function deselectAllLists(){
-    for (const child of listList.children){
+function deselectAllLists() {
+    for (const child of listList.children) {
         child.dataset.selected = false
     }
 }
@@ -75,13 +63,17 @@ function persistChanges() {
     localStorage.setItem("lists", JSON.stringify(lists));
 }
 
-function clearTodos(){
+function clearTodos() {
     todos = []
     persistChanges()
-    render()
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild)
+    }
 }
-function clearLists(){
+function clearLists() {
     lists = []
     persistChanges()
-    render()
+    while (listList.firstChild) {
+        listList.removeChild(listList.firstChild)
+    }
 }
