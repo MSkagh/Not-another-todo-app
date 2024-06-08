3296679b2110d8c24afd949c60bf9b361862ebcc
Martin Skagh <martin.skagh@gmail.com>
2024-06-08 23:44:56 +0200

    I need to rethink this, I´m so hell bent on trying to abstract it further than neccessary. Will try again at another time

--- script/list.js
+++ script/list.js
@@ -1,8 +1,23 @@
 export class List{
     constructor(name){
-        this.id = crypto.randomUUID()
-        this.name = name
-        this.completed = false,
-        this.creationDate = new Date(Date.now())
+        this.id = crypto.randomUUID();
+        this.name = name;
+        this.completed = false;
+        this.creationDate = new Date(Date.now());
+        this.tasks = [];
+        this.selected = true
+    }
+
+    getNode(){
+        const li = document.createElement("li");
+        li.dataset.selected = this.selected;
+        li.innerHTML = this.name
+        li.addEventListener("click", ()=> this.onClick())
+        return li
+    }
+
+    onClick(){
+        this.selected = true
+        console.log(this)
     }
 }
--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
 import { List } from "./list.js";
 import { Todo } from "./todo.js";
+
+//USER INPUTS
 const todoInput = document.getElementById("new-todo-input");
-const addTodoButton = document.getElementById("add-todo-button");
 const listInput = document.getElementById("new-list-input");
+const addTodoButton = document.getElementById("add-todo-button");
 const addListButton = document.getElementById("add-list-button");
 
 const deleteTodosButton = document.getElementById("delete-todos");
@@ -19,45 +21,54 @@ addListButton.addEventListener("click", () => addListButtonAction());
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 let lists = JSON.parse(localStorage.getItem("lists")) || [];
+let selectedList = document.querySelector("[data-selected=true]")
 
-function reRender(){
+function render(){
+    
     while(todoList.firstChild){
         todoList.removeChild(todoList.firstChild)
     }
     while(listList.firstChild){
         listList.removeChild(listList.firstChild)
     }
-    for (const item of todos){
-        const li = document.createElement("li");
-        li.innerHTML = item
-        todoList.appendChild(li);
-    }
-    
     for (const item of lists){
-        const li = document.createElement("li");
+        const li = document.createElement("li")
         li.innerHTML = item
         listList.appendChild(li);
     }
+    selectedList = document.querySelector("[data-selected=true]")
+    console.log(selectedList.innerHTML)
 }
-reRender()
+render()
 const addTodoButtonAction = () => {
-    const li = document.createElement("li");
-    li.innerHTML = todoInput.value;
-    todoList.appendChild(li);
-    todos.push(todoInput.value);
-    persistChanges();
-    todoInput.value = "";
+    console.log(selectedList)
 }
 
 const addListButtonAction = () => {
-    const li = document.createElement("li");
-    li.innerHTML = listInput.value;
+    const name = listInput.value
+    const li = document.createElement("li")
+    li.addEventListener("click", (e) => onListClick(e))
+    li.dataset.selected = true
+    li.dataset.name = name
     listList.appendChild(li);
-    lists.push(listInput.value);
+    lists.unshift(name);
     persistChanges();
     listInput.value = "";
+    render();
 }
 
+function onListClick(e){
+    for (const li of listList){
+        li.dataset.selected = false
+    }
+    e.target.dataset.selected = true
+}
+
+function deselectAllLists(){
+    for (const child of listList.children){
+        child.dataset.selected = false
+    }
+}
 
 function persistChanges() {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -67,10 +78,10 @@ function persistChanges() {
 function clearTodos(){
     todos = []
     persistChanges()
-    reRender()
+    render()
 }
 function clearLists(){
     lists = []
     persistChanges()
-    reRender()
+    render()
 }
